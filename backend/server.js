77275fce65b8e93b6a101eef32621cb72aaff80c
Zavista <84294353+Zavista@users.noreply.github.com>
2025-03-05 21:16:51 -0500

    simple database setup

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-
 import authRoutes from "./routes/auth.route.js";
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
+const PORT = ENV_VARS.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
@@ -18,6 +20,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/auth", authRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server started at http://localhost:3000");
+  connectDB();
 });
