e544c21cdd2030874f597f108c58fc872fe4a516
Zavista <84294353+Zavista@users.noreply.github.com>
2025-03-09 15:33:50 -0400

    Added login/logout logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -81,9 +81,50 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("Login Route");
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "All fields are required", success: false });
+    }
+
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Invalid credentials", success: false });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ message: "Invalid credentials", success: false });
+    }
+
+    generateToken(user._id, res);
+    const userResponse = user.toObject();
+    userResponse.password = "";
+
+    res.status(201).json({
+      message: "User logged in successfully",
+      success: true,
+      user: { userResponse },
+    });
+  } catch (error) {
+    console.log("Error in login controller: ", error);
+    res.status(500).json({ message: "Internal server error", success: false });
+  }
 };
 
 export const logout = async (req, res) => {
-  res.send("Logout Route");
+  try {
+    res.clearCookie("jwt-netflix");
+    res.status(200).json({ message: "Logged out successfully", success: true });
+  } catch (error) {
+    console.log("Error in logout controller: ", error);
+    res.status(500).json({ message: "Internal server error", success: false });
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,12 @@ import morgan from "morgan";
 import authRoutes from "./routes/auth.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = ENV_VARS.PORT || 3000;
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
